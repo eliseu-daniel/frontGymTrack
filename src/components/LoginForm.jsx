bb90d26db611a1c4718380bdefa87dff95150678
eliseu <eliseudaniel29@gmail.com>
2025-04-14 12:30:24 -0300

    css: titulo principal

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,18 +4,20 @@ import './LoginForm.css'
 export function LoginForm() {
     return (
         <div className="namePrincipal">
-            <h1>GYMTRACK</h1>
-            <div className="form">
+            <h1 className="titulo">GYMTRACK</h1>
+            <main className="form">
                 <div className="form form-inputs">
-                    <h1>LOGIN</h1>
+                    <div className="sub-titulo">
+                        <h1 className="sub">LOGIN</h1>
+                    </div>
                     <label htmlFor="e-mail">E-mail</label>
                     <input type="email" id="email" name="email" placeholder="E-mail" />
                     <label htmlFor="senha">Senha</label>
                     <input type="password" name="senha" id="senha" placeholder="Senha" />
                     <input type="submit" value="ENTRAR" />
-                    <a href="Cadastre.jsx">Cadastre-se</a>
+                    <a className="sub" href="Cadastre.jsx">Cadastre-se</a>
                 </div>
-            </div>
+            </main>
         </div>
     )
 }
